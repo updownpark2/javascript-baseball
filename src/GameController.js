626b97b15feed9700f80df09e2759ec5125918c8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-28 14:28:25 +0900

    feat(Controller): 컴퓨터의 값과 유저의 입력값을 비교 후 값을 리턴하는 함수 호출

--- src/GameController.js
+++ src/GameController.js
@@ -1,15 +1,30 @@
 const UserOutput = require("./UserOutput");
 const UserInput = require("./UserInput");
 const Validation = require("./Validation");
+const GameRule = require("./GameRule");
+const Computer = require("./Computer");
 
 const validation = new Validation();
+const gameRule = new GameRule(0, 0);
 
 class GameController {
+  getComputer() {
+    this.computer = Computer();
+  }
+
   gameStart() {
     UserOutput.start();
+    this.getComputer();
     UserInput.number((userInput) => {
+      console.log(this.computer);
       validation.checkNumber(userInput);
+      this.gameCalculation(userInput);
     });
+  }
+
+  gameCalculation(userInput) {
+    gameRule.totalCalculation(this.computer, userInput);
+    console.log(gameRule.result());
   }
 }
 
