a40f08295580233c8091f8d8a79aa5df08bae8b0
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-28 15:29:32 +0900

    feat: 게임 결과를 출력하는 함수 기능 추가

--- src/UserOutput.js
+++ src/UserOutput.js
@@ -4,6 +4,21 @@ const UserOutput = {
   start: () => {
     Console.print(`숫자야구 게임을 시작합니다.`);
   },
+
+  showResult: (result) => {
+    if (result[0] === 0 && result[1] === 0) {
+      Console.print(`낫씽`);
+    }
+    if (result[0] !== 0 && result[1] === 0) {
+      Console.print(`${result[0]}스트라이크`);
+    }
+    if (result[0] === 0 && result[1] !== 0) {
+      Console.print(`${result[1]}볼`);
+    }
+    if (result[0] !== 0 && result[1] !== 0) {
+      Console.print(`${result[1]}볼 ${result[0]}스트라이크`);
+    }
+  },
 };
 
 module.exports = UserOutput;
