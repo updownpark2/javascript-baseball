df52da928abf11f9518ce558718286464b8cbdf8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-28 15:42:19 +0900

    feat: 유저에게 재시작/종료 입력을 받는 함수 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -2,6 +2,15 @@ const { Console } = require("@woowacourse/mission-utils");
 
 const UserInput = {
   number: (callback) => {
-    Console.readline(`숫자를 입력해주세요`, callback);
+    Console.readLine(`숫자를 입력해주세요`, callback);
+  },
+
+  retry: (callback) => {
+    Console.readLine(
+      `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
+      callback
+    );
   },
 };
+
+module.exports = UserInput;
