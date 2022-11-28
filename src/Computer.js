cbd227c75bb1f1d60c9753eb39a05afc02f24bd9
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-28 14:26:33 +0900

    refactor: 컴퓨터의 랜덤 값 타입 string으로 받아오도록 변경

--- src/Computer.js
+++ src/Computer.js
@@ -1,7 +1,8 @@
+const { Random } = require("@woowacourse/mission-utils");
 function Computer() {
   const computer = [];
   while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    const number = String(Random.pickNumberInRange(1, 9));
     if (!computer.includes(number)) {
       computer.push(number);
     }
